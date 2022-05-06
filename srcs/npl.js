b90609ae269bc0fbb5b9066c140008e569fe60a1
bchancerel <benjamin.chancerel@nopow-link.com>
2022-05-06 18:12:44 +0200

    FEAT: methodes requetes

--- srcs/npl.js
+++ srcs/npl.js
@@ -4,9 +4,26 @@ let NPL = {
         const CURRENT_URL = $(location).attr('href')
         const SLUG        = window.location.pathname
 
+        var pk = api_key.trim()
+
         console.log(SLUG)
         console.log(CURRENT_URL)
         console.log(Axios)
+    },
+
+    get_Data() {
+        axios.get('/api/public/plugin/2b18fd7065f98e0ff6f46789b61a8ab46fb14bdb91dd99dcb8ac2b229d0bdb51/collect/?slug=/this/is/an/example?test=True')
+            .then(response => response.data)
+            console.log(response.data)
+            if (response.data.certify == false) {
+                certify()
+            }
+    },
+
+    certify() {
+        axios.post('/api/public/plugin/d88f561f37d97c0f0146b46b83f75ce33694db9302327e77ccac5d8055f7a4fc/certify')
+            .then(response => response.data)
+            console.log(response.data)
     }
 }
 
@@ -33,13 +50,6 @@ export default NPL
 //     slug        : window.location.pathname
 
 // }
-
-// function get_Data() {
-//     axios.get('/api/public/plugin/2b18fd7065f98e0ff6f46789b61a8ab46fb14bdb91dd99dcb8ac2b229d0bdb51/collect/?slug=/this/is/an/example?test=True')
-//         .then(response => response.data)
-//         console.log(response.data)
-// }
-
 // function certify() {
 //     axios.post('/api/public/plugin/d88f561f37d97c0f0146b46b83f75ce33694db9302327e77ccac5d8055f7a4fc/certify')
 //         .then(response => response.data)
