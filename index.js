e40bbe8a847c899200957f7e45555feffeeb9dfa
bchancerel <benjamin.chancerel@nopow-link.com>
2022-05-13 15:25:24 +0200

    FEAT: Preparation pour difusion

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-import JQuery from 'jquery'
 import Axios from 'axios'
 import trim from "trim"
 import core_js from "core-js"
-
-// Load JQuery globaly
-const strftime  = require('strftime')
-const $         = JQuery
-window.jQuery   = $
 
 // Setup Axios
 Axios.defaults.headers = {'X-Requested-With': 'XMLHttpRequest'}
@@ -14,5 +8,5 @@ Axios.defaults.headers = {'X-Requested-With': 'XMLHttpRequest'}
 // Librairy entrypoint
 import NPL from './srcs/npl.js'
 
-export { JQuery, Axios, NPL, $, trim, core_js }
+export { Axios, NPL, trim, core_js }
 
